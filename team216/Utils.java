654c0b5c985c2777ea9e7412dd2b46513415d785
David Chouinard <david.chouinard@me.com>
2013-01-24 11:26:01 -0500

    Added secure communication framework to Utils

--- team216/Utils.java
+++ team216/Utils.java
@@ -8,6 +8,15 @@ public class Utils
     public static final Direction dirByOrd[] = Direction.values();
     public static final int dirOrdMask = (8 - 1);
 
+    // These are just arbitrary values, feel free to change
+    private static final int CHANNEL_1 = 235;
+    private static final int CHANNEL_2 = 61234;
+    private static final int BACKUP_CHANNEL_1 = 9234;
+    private static final int BACKUP_CHANNEL_2 = 40129;
+
+    // We use this arbitrary seed to add entropy to messaging data
+    private static final int SECRET_COMMUNICATION_SEED = 14212;
+
     public static int ceilDiv(int a, int b)
     {
         if (b == 0) return 1;
@@ -48,5 +57,40 @@ public class Utils
     		( 0.8 * (factor - distTwoPoints(p, hq)) + 
     		0.2 * (factor - distToLineBetween(p, hq, evilHq)) )
     		/ factor;
+    }
+
+    public static void broadcastMessage(RobotController rc, int data) {
+        // We add a constant value (channel number times a seed) to every message and
+        // broadcast the data on four distinct channels (two main and two for backup)
+        // Note that the addition might cause integer overflow, but this is fine since
+        // Java ints wrap around
+
+        try {
+            rc.broadcast(CHANNEL_1, data + CHANNEL_1 * SECRET_COMMUNICATION_SEED);
+            rc.broadcast(CHANNEL_2, data + CHANNEL_2 * SECRET_COMMUNICATION_SEED);
+
+            // Backup broadcast, if we detect that the main channels are getting fudged
+            rc.broadcast(BACKUP_CHANNEL_1, data + BACKUP_CHANNEL_1 * SECRET_COMMUNICATION_SEED);
+            rc.broadcast(BACKUP_CHANNEL_2, data + BACKUP_CHANNEL_2 * SECRET_COMMUNICATION_SEED);
+        } catch(GameActionException e) { e.printStackTrace(); }
+    }
+
+    public static int readMessage(RobotController rc) {
+        try {
+            int data = rc.readBroadcast(CHANNEL_1) - CHANNEL_1 * SECRET_COMMUNICATION_SEED;
+
+            if (data == rc.readBroadcast(CHANNEL_2) - CHANNEL_2 * SECRET_COMMUNICATION_SEED)
+                return data;
+
+            // somebody's been fucking with our messages, try the backup channel
+            data = rc.readBroadcast(BACKUP_CHANNEL_1) - BACKUP_CHANNEL_1 * SECRET_COMMUNICATION_SEED;
+
+            if (data == rc.readBroadcast(BACKUP_CHANNEL_2) - BACKUP_CHANNEL_2 * SECRET_COMMUNICATION_SEED)
+                return data;
+
+        } catch(GameActionException e) { e.printStackTrace(); }
+
+        // both the main data and the backup data are corrupted, give up
+        return 0;
     }
 }
